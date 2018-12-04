47cde6d38e0d6dac9ef8459ac130ba588c0f7aa1
sepehrjavid <javid.sepehr77@gmail.com>
2018-12-04 17:54:35 +0330

    start

---
+++ cmake-build-debug/CMakeFiles/Cpp_java_game_engin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp_java_game_engin.dir/main.cpp.o"
+  "Cpp_java_game_engin.pdb"
+  "Cpp_java_game_engin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp_java_game_engin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Cpp_java_game_engin.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Cpp_java_game_engin.
+# This may be replaced when dependencies are built.
